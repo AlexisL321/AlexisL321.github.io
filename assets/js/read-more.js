c50e77185a31b566272c45e5383484e378884548
AlexisL321 <liu2256@wisc.edu>
2024-10-05 20:31:36 -0500

    read more function

--- assets/js/read-more.js
+++ assets/js/read-more.js
@@ -1,38 +1,39 @@
 function setupReadMore() {
-  const readMoreContainers = document.querySelectorAll('.read-more-container');
+  const readMoreContainers = document.querySelectorAll(".read-more-container");
 
-  readMoreContainers.forEach(container => {
-	const maxLength = parseInt(container.getAttribute('max-length'), 10);
-	if (maxLength == NaN) maxLength = 100; //if max-length is not specified
+  readMoreContainers.forEach((container) => {
+    const maxLength = parseInt(container.getAttribute("max-length"), 10);
+    if (maxLength == NaN) maxLength = 100; //if max-length is not specified
 
-	const fullText = readMoreContent.textContent.trim();
-	if (fullText.length > maxLength) {
-const show = fullText.slice(0, maxLength);
-const hide = fullText.slice(maxLength);
-container.innerHTML = `${show}<span class="dots">...</span><span
+    const fullText = readMoreContent.textContent.trim();
+    if (fullText.length > maxLength) {
+      const show = fullText.slice(0, maxLength);
+      const hide = fullText.slice(maxLength);
+      container.innerHTML = `${show}<span class="dots">...</span><span
 class="hidden-content">${hide}</span><span class="read-more-btn">Read
  More</span>`;
 
- const readMoreBtn = container.querySelector('.read-more-btn');
- const dots = container.querySelector('dots');
- const hiddenText = container.querySelector('hidden-content');
+      const readMoreBtn = container.querySelector(".read-more-btn");
+      const dots = container.querySelector("dots");
+      const hiddenText = container.querySelector("hidden-content");
 
-readMoreBtn.addEventListener('click', ()=>{
-if (hiddenText.style.disply==='none' || hiddenText.style.display==='') {
-hiddenText.style.display = 'inline';
-dots.style.display='none';
-readMoreBtn.textContent = 'Read Less';
+      readMoreBtn.addEventListener("click", () => {
+        if (
+          hiddenText.style.disply === "none" ||
+          hiddenText.style.display === ""
+        ) {
+          hiddenText.style.display = "inline";
+          dots.style.display = "none";
+          readMoreBtn.textContent = "Read Less";
+        } else {
+          hiddenText.style.display = "none";
+          dots.style.display = "inline";
+          readMorebtn.textContent = "Read More";
+        }
+      });
+    }
+  });
 }
-else {
-hiddenText.style.display = 'none';
-dots.style.display = 'inline';
-readMorebtn.textContent = 'Read More';
-}
-});
-	}
-	});
-	}
 
 // Export the function so it can be used in the HTML file
-export { setupReadMore };
-
+module.exports= { setupReadMore };
