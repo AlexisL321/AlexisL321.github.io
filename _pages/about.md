a6c7bf4e471c4ea5a19043d7acf2ec345594f929
AlexisL321 <liu2256@wisc.edu>
2024-10-05 23:45:33 -0500

    read more function

--- _pages/about.md
+++ _pages/about.md
@@ -2,12 +2,13 @@
 permalink: /
 title: "Hello, World~"
 author_profile: true
-redirect_from: 
+redirect_from:
   - /about/
   - /about.html
 ---
+
 I am a fourth-year undergraduate at UW-Madison. My research interest is in
-**computer graphics** and **computational mathematics**. I am especially interested in physics-based simulation, topology optimization, sparse matrix 
+**computer graphics** and **computational mathematics**. I am especially interested in physics-based simulation, topology optimization, sparse matrix
 algorithms and all kinds of numerical methods and matrix factorization
 techniques. I am also constantly exploring new topics within computer graphics!
 
@@ -19,8 +20,8 @@ internship in spring and summer 2025.
 
 Feel free to email me at liu2256 [at] wisc [dot] edu.
 
-Side note
-------
+## Side note
+
 I am a huge history fan. Check out the
 [History](https://AlexisL321.github.io/history/) page (currently under
 construction) on my website!
--- assets/js/read-more.js
+++ assets/js/read-more.js
@@ -5,18 +5,18 @@ function setupReadMore() {
     const maxLength = parseInt(container.getAttribute("max-length"), 10);
     if (maxLength == NaN) maxLength = 100; //if max-length is not specified
 
-	const text = container.querySelector('.read-more-text');
-	const readMoreBtn = container.querySelector('.read-more-btn');
+    const text = container.querySelector(".read-more-text");
+    const readMoreBtn = container.querySelector(".read-more-btn");
 
     const fullText = text.innerHTML;
     if (fullText.length > maxLength) {
-	  text.innerHTML = truncateHTML(fullText, maxLength);
-	  let isExpanded = false;
+      text.innerHTML = truncateHTML(fullText, maxLength);
+      let isExpanded = false;
 
       //const show = fullText.slice(0, maxLength);
       //const hide = fullText.slice(maxLength);
       //container.innerHTML = `${show}<span class="dots">...</span><span class="hidden-content">${hide}</span><span class="read-more-btn">Read More</span>`;
-	  //container.innerHTML += `<span class="dots">...</span><span class="read-more-btn"> Read More</span>`;
+      //container.innerHTML += `<span class="dots">...</span><span class="read-more-btn"> Read More</span>`;
 
       //const readMoreBtn = container.querySelector(".read-more-btn");
       //const dots = container.querySelector("dots");
@@ -26,33 +26,35 @@ function setupReadMore() {
         if (
           //hiddenText.style.disply === "none" ||
           //hiddenText.style.display === ""
-		  !isExpanded //hasn't been expanded
+          !isExpanded //hasn't been expanded
         ) {
-		  text.innerHTML = fullText;
-		  //This does not work because innerHTML is always getting changed
-		  //and thus the previous btn is overwritten
-		  //text.innerHTML += `<span class="read-more-btn">Read More</span>`;
+          text.innerHTML = fullText;
+          //This does not work because innerHTML is always getting changed
+          //and thus the previous btn is overwritten
+          //text.innerHTML += `<span class="read-more-btn">Read More</span>`;
           //hiddenText.style.display = "inline";
           //dots.style.display = "none";
           readMoreBtn.textContent = "Read Less";
         } else {
-		  text.innerHTML = truncateHTML(fullText, maxLength);
-		  //text.innerHTML += `<span class="dots">...</span><span class="read-more-btn"> Read More</span>`;
+          text.innerHTML = truncateHTML(fullText, maxLength);
+          //text.innerHTML += `<span class="dots">...</span><span class="read-more-btn"> Read More</span>`;
           //hiddenText.style.display = "none";
           //dots.style.display = "inline";
           readMoreBtn.textContent = "...Read More";
         }
-		isExpanded = !isExpanded;
+        isExpanded = !isExpanded;
       });
+    } else {
+      readMoreBtn.style.display = "none";
     }
   });
 }
 
-function truncateHTML (html, charLimit) {
-  let div = document.createElement('div');
+function truncateHTML(html, charLimit) {
+  let div = document.createElement("div");
   div.innerHTML = html;
 
-  let content = '';
+  let content = "";
   let length = 0;
 
   function traverse(node) {
@@ -66,11 +68,13 @@ function truncateHTML (html, charLimit) {
         length += text.length;
       }
     } else if (node.nodeType === Node.ELEMENT_NODE) {
-      let tagOpen = `<${node.tagName.toLowerCase()}${Array.from(node.attributes).map(attr => ` ${attr.name}="${attr.value}"`).join('')}>`;
+      let tagOpen = `<${node.tagName.toLowerCase()}${Array.from(node.attributes)
+        .map((attr) => ` ${attr.name}="${attr.value}"`)
+        .join("")}>`;
       let tagClose = `</${node.tagName.toLowerCase()}>`;
-      
+
       content += tagOpen;
-      Array.from(node.childNodes).forEach(child => {
+      Array.from(node.childNodes).forEach((child) => {
         if (length < charLimit) {
           traverse(child);
         }
